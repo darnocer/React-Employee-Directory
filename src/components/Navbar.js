990f383e2d09df34f5feae68b683117ed684cd2f
darnocer <darnocer@gmail.com>
2020-08-03 17:12:32 -0600

    navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <span className="navbar-brand mx-auto h1">Employee Directory</span>
+    </nav>
+  );
+}
+
+export default Navbar;
